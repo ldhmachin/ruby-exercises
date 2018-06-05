8888f3ae76cbcd889bb3b4d202941664fb64b3c5
Louis Machin <ldhmachin@gmail.com>
2018-06-05 09:43:36 +0100

    Update even_fibonacci.rb

--- even_fibonacci.rb
+++ even_fibonacci.rb
@@ -1,8 +1,9 @@
 def fibonacci(n)
   return n if (0..1).include? n
-  ( fibonacci(n-1) + fibonnaci(n-2) )
+  ( fibonacci(n-1) + fibonacci(n-2) )
 end
 
 (0..100).each_with_index do |item,index|
-  puts fibonacci(index).to_i if fibonacci(index).to_i.even?
+  num = fibonacci(index).to_i
+  puts num if num.even? & (num < 4000000)
 end
